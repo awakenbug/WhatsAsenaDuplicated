e7e5dc51443acdc1d4e032c408db80e3043eb706
awakenbug <71434191+awakenbug@users.noreply.github.com>
2021-04-23 03:11:30 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *4LPH4_CR4CKZ*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *4LPH4_CR4CKZ*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *4LPH4_CR4CKZ*:  ```" + msg + "```"
 }
 
 
